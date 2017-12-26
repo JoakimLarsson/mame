27378c4268e2ca6d36b17f21c4c4f2ead2ea723e
Joakim Larsson Edstrom <joakimlarsson42@gmail.com>
2017-12-26 10:32:04 +0100

    WFC-1 on the move, nothing compiles atm

--- src/devices/bus/vme/vme_fcwfc.cpp
+++ src/devices/bus/vme/vme_fcwfc.cpp
@@ -112,6 +112,7 @@
 
 #include "emu.h"
 #include "cpu/mcs48/mcs48.h"
+#include "machine/wd11c00_17.h"
 #include "vme_fcwfc.h"
 
 //#define LOG_GENERAL (1U <<  0)
@@ -132,34 +133,70 @@
 
 #define TODO "Driver for WD1015, WD2927, WD1014 and WD1010 needed\n"
 #define WD1015_TAG      "j36"
+#define HLE 0
 
 //**************************************************************************
 //	GLOBAL VARIABLES
 //**************************************************************************
 
-//const device_type VME_FCWFC1 = device_creator<vme_fcwfc1_card_device>;
 DEFINE_DEVICE_TYPE(VME_FCWFC1, vme_fcwfc1_card_device, "fcwfc1", "Force Computer SYS68K/WFC-1 Floppy and Winchester Controller Board");
 
 //-------------------------------------------------
-//  ADDRESS_MAP( wd1015_io )
+//  ADDRESS_MAP
 //-------------------------------------------------
 
-static ADDRESS_MAP_START( wd1015_io, AS_IO, 8, vme_fcwfc1_card_device )
-#if 0 // from wdxt_gen.cpp TODO: Check schematics and hook up stuff accordingly
+DEVICE_ADDRESS_MAP_START( map, 8, vme_fcwfc1_card_device )
+#if HLE
+	AM_RANGE(0x00, 0x00) AM_WRITE(complete_irq_vector_w)
+	AM_RANGE(0x01, 0x01) AM_READ(data_r) AM_WRITE(data_w)
+	AM_RANGE(0x02, 0x02) AM_WRITE(data_req_irq_w)
+	AM_RANGE(0x03, 0x03) AM_READ(error_r) AM_WRITE(precomp_w)
+	AM_RANGE(0x05, 0x05) AM_READ(sector_count_r) AM_WRITE(sector_count_w)
+	AM_RANGE(0x07, 0x07) AM_READ(sector_number_r) AM_WRITE(sector_number_w)
+	AM_RANGE(0x09, 0x09) AM_READ(cylinder_low_r) AM_WRITE(cylinder_low_w)
+	AM_RANGE(0x0b, 0x0b) AM_READ(cylinder_high_r) AM_WRITE(cylinder_high_w)
+	AM_RANGE(0x0d, 0x0d) AM_READ(size_drive_head_r) AM_WRITE(size_drive_head_w)
+	AM_RANGE(0x0f, 0x0f) AM_READ(status_r) AM_WRITE(command_w)
+#else
+	AM_RANGE(0x0000, 0x000f) AM_READWRITE(dpram_r, dpram_w)
+#if 0
+	/* All these lines are ripped from wdxt_gen.cpp so need to be adjusted before being enabled in LLE */
 	AM_RANGE(0x00, 0xff) AM_DEVREADWRITE(WD11C00_17_TAG, wd11c00_17_device, read, write)
 	AM_RANGE(MCS48_PORT_T0, MCS48_PORT_T0) AM_READ(wd1015_t0_r)
 	AM_RANGE(MCS48_PORT_T1, MCS48_PORT_T1) AM_READ(wd1015_t1_r)
 	AM_RANGE(MCS48_PORT_P1, MCS48_PORT_P1) AM_READWRITE(wd1015_p1_r, wd1015_p1_w)
 	AM_RANGE(MCS48_PORT_P2, MCS48_PORT_P2) AM_READWRITE(wd1015_p2_r, wd1015_p2_w)
 #endif
+#endif
 ADDRESS_MAP_END
+
+FLOPPY_FORMATS_MEMBER( vme_fcwfc1_card_device::fcwfc1_floppy_formats )
+	FLOPPY_IMD_FORMAT
+FLOPPY_FORMATS_END
+
+static SLOT_INTERFACE_START( fcwfc1_floppies )
+	SLOT_INTERFACE( "525sd", FLOPPY_525_SD )
+SLOT_INTERFACE_END
 
 /*
  * Machine configuration
  */
 MACHINE_CONFIG_MEMBER (vme_fcwfc1_card_device::device_add_mconfig)
+#if HLE
+#else
+	MCFG_FLOPPY_DRIVE_ADD("fdc:0", fcwfc1_floppies, "525sd", vme_fcwfc1_card_device::fcwfc1_floppy_formats)
+	MCFG_FLOPPY_DRIVE_ADD("fdc:1", fcwfc1_floppies, "525sd", vme_fcwfc1_card_device::fcwfc1_floppy_formats)
+#if 0
 	MCFG_CPU_ADD(WD1015_TAG, I8049, 5000000)
-	MCFG_CPU_IO_MAP(wd1015_io)
+	MCFG_CPU_PROGRAM_MAP(wd1015_mem)
+	MCFG_MCS48_PORT_T0_IN_CB(DEVREADLINE(WD11C00_17_TAG, wd11c00_17_device, busy_r))
+	MCFG_MCS48_PORT_T1_IN_CB(READLINE(wdxt_gen_device, wd1015_t1_r))
+	MCFG_MCS48_PORT_P1_IN_CB(READ8(wdxt_gen_device, wd1015_p1_r))
+	MCFG_MCS48_PORT_P1_OUT_CB(WRITE8(wdxt_gen_device, wd1015_p1_w))
+	MCFG_MCS48_PORT_P2_IN_CB(READ8(wdxt_gen_device, wd1015_p2_r))
+	MCFG_MCS48_PORT_P2_OUT_CB(WRITE8(wdxt_gen_device, wd1015_p2_w))
+#endif
+#endif
 MACHINE_CONFIG_END
 
 //-------------------------------------------------
@@ -183,12 +220,6 @@ ROM_START (fcwfc1)
 	ROM_REGION( 0x800, WD1015_TAG, 0 )
 	ROM_LOAD( "WD1015-10.BIN", 0x000, 0x800, CRC(85dfe326) SHA1(f54803da3668193a3470ee0e24e3ea47ae605ec3) )
 ROM_END
-
-const tiny_rom_entry *vme_fcwfc1_card_device::device_rom_region() const
-{
-	LOG("%s\n", FUNCNAME);
-	return ROM_NAME( fcwfc1 );
-}
 
 //**************************************************************************
 //  LIVE DEVICE
@@ -215,9 +246,14 @@ void vme_fcwfc1_card_device::device_start()
 
 	uint32_t base = 0xFCB01000; // Miniforce default base + offset 0-f TODO: Make configurable
 
+#if HLE
 	m_vme->install_device(vme_device::A24_SC, base      , base + 0x0f, // Dual ported RAM A24:D8
 						  read8_delegate(FUNC(vme_fcwfc1_card_device::not_implemented_r), this),
 						  write8_delegate(FUNC(vme_fcwfc1_card_device::not_implemented_w), this), 0xffffffff);
+#else
+	// Dual ported RAM A24:D8
+	m_vme->install_device(vme_device::A24_SC, base, base + 0x0f, &vme_fcwfc1_card_device::map, 8, 0xffffffff);
+#endif
 }
 
 void vme_fcwfc1_card_device::device_reset()
@@ -225,7 +261,21 @@ void vme_fcwfc1_card_device::device_reset()
 	LOG("%s\n", FUNCNAME);
 }
 
-READ8_MEMBER (vme_fcwfc1_card_device::not_implemented_r){
+READ8_MEMBER (vme_fcwfc1_card_device::dpram_r)
+{
+	uint8_t data = 0;
+
+	LOGR("%s: %02x\n", FUNCNAME, data); 
+	return data;
+}
+
+WRITE8_MEMBER (vme_fcwfc1_card_device::dpram_w)
+{
+	LOGSETUP("%s %02x <= %02x\n", FUNCNAME, offset, data); 
+}
+
+READ8_MEMBER (vme_fcwfc1_card_device::not_implemented_r)
+{
 	static int been_here = 0;
 	if (!been_here++){
 		logerror(TODO);
@@ -234,7 +284,8 @@ READ8_MEMBER (vme_fcwfc1_card_device::not_implemented_r){
 	return (uint8_t) 0;
 }
 
-WRITE8_MEMBER (vme_fcwfc1_card_device::not_implemented_w){
+WRITE8_MEMBER (vme_fcwfc1_card_device::not_implemented_w)
+{
 	static int been_here = 0;
 	if (!been_here++){
 		logerror(TODO);
@@ -242,3 +293,24 @@ WRITE8_MEMBER (vme_fcwfc1_card_device::not_implemented_w){
 	}
 	return;
 }
+
+#if 0
+WRITE8_MEMBER (complete_irq_vector_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+READ8_MEMBER (data_r){ uint8_t data = 0; LOGR("%s: %02x\n", FUNCNAME, data); return data; }
+WRITE8_MEMBER (data_req_irq_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+WRITE8_MEMBER (data_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+READ8_MEMBER (error_r){ uint8_t data = 0; LOGR("%s: %02x\n", FUNCNAME, data); return data; }
+WRITE8_MEMBER (precomp_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+READ8_MEMBER (sector_count_r){ uint8_t data = 0; LOGR("%s: %02x\n", FUNCNAME, data); return data; }
+WRITE8_MEMBER (sector_count_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+READ8_MEMBER (sector_number_r){ uint8_t data = 0; LOGR("%s: %02x\n", FUNCNAME, data); return data; }
+WRITE8_MEMBER (sector_number_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+READ8_MEMBER (cylinder_low_r){ uint8_t data = 0; LOGR("%s: %02x\n", FUNCNAME, data); return data; }
+WRITE8_MEMBER (cylinder_low_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+READ8_MEMBER (cylinder_high_r){ uint8_t data = 0; LOGR("%s: %02x\n", FUNCNAME, data); return data; }
+WRITE8_MEMBER (cylinder_high_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+READ8_MEMBER (size_drive_head_r){ uint8_t data = 0; LOGR("%s: %02x\n", FUNCNAME, data); return data; }
+WRITE8_MEMBER (size_drive_head_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+READ8_MEMBER (status_r){ uint8_t data = 0; LOGR("%s: %02x\n", FUNCNAME, data); return data; }
+WRITE8_MEMBER (command_w){ LOGSETUP("%s %02x\n", FUNCNAME, data); }
+#endif
--- src/devices/bus/vme/vme_fcwfc.h
+++ src/devices/bus/vme/vme_fcwfc.h
@@ -5,6 +5,8 @@
 #pragma once
 
 #include "bus/vme/vme.h"
+#include "machine/wd_fdc.h"
+#include "formats/imd_dsk.h"
 
 DECLARE_DEVICE_TYPE(VME_FCWFC1, vme_fcwfc1_card_device);
 
@@ -13,23 +15,54 @@ class vme_fcwfc1_card_device :
 	,public device_vme_card_interface
 {
 public:
-	vme_fcwfc1_card_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock);
 	vme_fcwfc1_card_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+protected:
+	vme_fcwfc1_card_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock);
 
-	// optional information overrides
+	// device-level overrides
 	virtual void device_add_mconfig(machine_config &config) override;
-	virtual const tiny_rom_entry *device_rom_region() const override;
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+#if 0
+	DECLARE_WRITE8_MEMBER (complete_irq_vector_w);
+	DECLARE_READ8_MEMBER (data_r);
+	DECLARE_WRITE8_MEMBER (data_req_irq_w);
+	DECLARE_WRITE8_MEMBER (data_w);
+	DECLARE_READ8_MEMBER (error_r);
+	DECLARE_WRITE8_MEMBER (precomp_w);
+	DECLARE_READ8_MEMBER (sector_count_r);
+	DECLARE_WRITE8_MEMBER (sector_count_w);
+	DECLARE_READ8_MEMBER (sector_number_r);
+	DECLARE_WRITE8_MEMBER (sector_number_w);
+	DECLARE_READ8_MEMBER (cylinder_low_r);
+	DECLARE_WRITE8_MEMBER (cylinder_low_w);
+	DECLARE_READ8_MEMBER (cylinder_high_r);
+	DECLARE_WRITE8_MEMBER (cylinder_high_w);
+	DECLARE_READ8_MEMBER (size_drive_head_r);
+	DECLARE_WRITE8_MEMBER (size_drive_head_w);
+	DECLARE_READ8_MEMBER (status_r);
+	DECLARE_WRITE8_MEMBER (command_w);
+#endif
 
 	/* Dummy driver routines */
 	DECLARE_READ8_MEMBER (not_implemented_r);
 	DECLARE_WRITE8_MEMBER (not_implemented_w);
 
-protected:
-	virtual void device_start() override;
-	virtual void device_reset() override;
+	/* Host interface*/
+	DECLARE_READ8_MEMBER (dpram_r);
+	DECLARE_WRITE8_MEMBER (dpram_w);
+	DECLARE_ADDRESS_MAP(map, 8);
+
+	required_device<wd2797_device> m_fdc;
+	required_device<floppy_connector> m_fdd0;
+	optional_device<floppy_connector> m_fdd1;
+	optional_device<floppy_connector> m_fdd2;
+	optional_device<floppy_connector> m_fdd3;
 
 private:
 	required_device<cpu_device> m_maincpu;
+	DECLARE_FLOPPY_FORMATS( fcwfc1_floppy_formats );
 };
 
 #endif // VME_FCWFC1_H
