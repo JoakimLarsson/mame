9cdf919e56412043a18c9ea649eccf4235e52b5e
Joakim Larsson Edstrom <Joakim.Edstrom@edstromious.se>
2019-11-26 14:15:22 +0100

    mc6844: device ripped out from swtpc09 now compiles

--- src/devices/machine/mc6844.cpp
+++ src/devices/machine/mc6844.cpp
@@ -101,11 +101,205 @@ void mc6844_device::device_reset()
 //  READ/WRITE HANDLERS
 //**************************************************************************
 
-READ8_MEMBER( mc6844_device::read )
+
+READ8_MEMBER( mc6844_device::m6844_r )
 {
-	return 0;
+	uint8_t result = 0;
+
+	// switch off the offset we were given
+	switch (offset)
+	{
+		// upper byte of address
+		case 0x00:
+		case 0x04:
+		case 0x08:
+		case 0x0c:
+			result = m_m6844_channel[offset / 4].address >> 8;
+			break;
+
+		// lower byte of address
+		case 0x01:
+		case 0x05:
+		case 0x09:
+		case 0x0d:
+			result = m_m6844_channel[offset / 4].address & 0xff;
+			break;
+
+		// upper byte of counter
+		case 0x02:
+		case 0x06:
+		case 0x0a:
+		case 0x0e:
+			result = m_m6844_channel[offset / 4].counter >> 8;
+			break;
+
+		// lower byte of counter
+		case 0x03:
+		case 0x07:
+		case 0x0b:
+		case 0x0f:
+			result = m_m6844_channel[offset / 4].counter & 0xff;
+			break;
+
+		// channel control
+		case 0x10:
+		case 0x11:
+		case 0x12:
+		case 0x13:
+			result = m_m6844_channel[offset - 0x10].control;
+
+			// A read here clears the 'DMA end' flag of the
+			// associated channel.
+			if (!machine().side_effects_disabled())
+			{
+				m_m6844_channel[offset - 0x10].control &= ~0x80;
+				if (m_m6844_interrupt & 0x80)
+					m6844_update_interrupt();
+			}
+			break;
+
+		// priority control
+		case 0x14:
+			result = m_m6844_priority;
+			break;
+
+		// interrupt control
+		case 0x15:
+			result = m_m6844_interrupt;
+			break;
+
+		// chaining control
+		case 0x16:
+			result = m_m6844_chain;
+			break;
+
+		// 0x17-0x1f not used
+		default: break;
+	}
+
+	return result & 0xff;
 }
 
-WRITE8_MEMBER( mc6844_device::write )
+
+WRITE8_MEMBER( mc6844_device::m6844_w )
 {
+	int i;
+
+	// switch off the offset we were given
+	switch (offset)
+	{
+		// upper byte of address
+		case 0x00:
+		case 0x04:
+		case 0x08:
+		case 0x0c:
+			m_m6844_channel[offset / 4].address = (m_m6844_channel[offset / 4].address & 0xff) | (data << 8);
+			break;
+
+		// lower byte of address
+		case 0x01:
+		case 0x05:
+		case 0x09:
+		case 0x0d:
+			m_m6844_channel[offset / 4].address = (m_m6844_channel[offset / 4].address & 0xff00) | (data & 0xff);
+			break;
+
+		// upper byte of counter
+		case 0x02:
+		case 0x06:
+		case 0x0a:
+		case 0x0e:
+			m_m6844_channel[offset / 4].counter = (m_m6844_channel[offset / 4].counter & 0xff) | (data << 8);
+			break;
+
+		// lower byte of counter
+		case 0x03:
+		case 0x07:
+		case 0x0b:
+		case 0x0f:
+			m_m6844_channel[offset / 4].counter = (m_m6844_channel[offset / 4].counter & 0xff00) | (data & 0xff);
+			break;
+
+		// channel control
+		case 0x10:
+		case 0x11:
+		case 0x12:
+		case 0x13:
+			m_m6844_channel[offset - 0x10].control = (m_m6844_channel[offset - 0x10].control & 0xc0) | (data & 0x3f);
+			break;
+
+		// priority control
+		case 0x14:
+			m_m6844_priority = data;
+
+			// update each channel
+			for (i = 0; i < 4; i++)
+			{
+				// if we're going active...
+				if (!m_m6844_channel[i].active && (data & (1 << i)))
+				{
+					// mark us active
+					m_m6844_channel[i].active = 1;
+
+					// set the DMA busy bit and clear the DMA end bit
+					m_m6844_channel[i].control |= 0x40;
+					m_m6844_channel[i].control &= ~0x80;
+
+					// set the starting address, counter, and time
+					m_m6844_channel[i].start_address = m_m6844_channel[i].address;
+					m_m6844_channel[i].start_counter = m_m6844_channel[i].counter;
+				}
+
+				// if we're going inactive...
+				else if (m_m6844_channel[i].active && !(data & (1 << i)))
+				{
+					//mark us inactive
+					m_m6844_channel[i].active = 0;
+				}
+			}
+			break;
+
+		// interrupt control
+		case 0x15:
+			m_m6844_interrupt = (m_m6844_interrupt & 0x80) | (data & 0x7f);
+			m6844_update_interrupt();
+			break;
+
+		// chaining control
+		case 0x16:
+			m_m6844_chain = data;
+			break;
+
+		// 0x17-0x1f not used
+		default: break;
+	}
+}
+
+void mc6844_device::m6844_update_interrupt()
+{
+	uint8_t interrupt = 0;
+
+	interrupt |= BIT(m_m6844_channel[0].control, 7) & BIT(m_m6844_interrupt, 0);
+	interrupt |= BIT(m_m6844_channel[1].control, 7) & BIT(m_m6844_interrupt, 1);
+	interrupt |= BIT(m_m6844_channel[2].control, 7) & BIT(m_m6844_interrupt, 2);
+	interrupt |= BIT(m_m6844_channel[3].control, 7) & BIT(m_m6844_interrupt, 3);
+
+	if (interrupt)
+	{
+		if (!(m_m6844_interrupt & 0x80))
+		{
+			// Set interrupt indication bit 7.
+			m_m6844_interrupt |= 0x80;
+			//swtpc09_irq_handler(DMAC_IRQ, ASSERT_LINE); // FIX: callback
+		}
+	}
+	else
+	{
+		if (m_m6844_interrupt & 0x80)
+		{
+			// Clear interrupt indication bit 7.
+			m_m6844_interrupt &= 0x7f;
+			// swtpc09_irq_handler(DMAC_IRQ, CLEAR_LINE); // FIX: callback
+		}
+	}
 }
--- src/devices/machine/mc6844.h
+++ src/devices/machine/mc6844.h
@@ -32,8 +32,8 @@ public:
 	mc6844_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
 
 	// I/O operations
-	DECLARE_WRITE8_MEMBER( write );
-	DECLARE_READ8_MEMBER( read );
+	DECLARE_READ8_MEMBER ( m6844_r );
+	DECLARE_WRITE8_MEMBER ( m6844_w );
 
 protected:
 	// device-level overrides
@@ -41,8 +41,47 @@ protected:
 	virtual void device_add_mconfig(machine_config &config) override;
 	virtual void device_start() override;
 	virtual void device_reset() override;
-};
 
+	/* channel_data structure holds info about each 6844 DMA channel */
+	struct m6844_channel_data
+	{
+		int active;
+		int address;
+		int counter;
+		// Channel control register.
+		//  bit 0: Read / Write mode
+		//  bit 1: Mode control B
+		//  bit 2: Mode control A
+		//  bit 3: Address up (0) / down (1).
+		//  bit 4: Not used
+		//  bit 5: Not used
+		//  bit 6: Busy / Ready. Read only. Set when request
+		//         made. Cleared when transfer completed.
+		//  bit 7: DMA end flag. Read only? Set when transfer
+		//         completed. Cleared when control register
+		//          read. Sets IRQ.
+		// Mode control A,B: 0,0 Mode2; 0,1 Mode 3; 1,0 Mode 0;
+		//                   1,1 Undefined.
+		uint8_t control;
+		int start_address;
+		int start_counter;
+	};
+
+	/* 6844 description */
+	m6844_channel_data m_m6844_channel[4];
+	uint8_t m_m6844_priority;
+	// Interrupt control register.
+	// Bit 0-3: channel interrupt enable, 1 enabled, 0 masked.
+	// Bit 4-6: unused
+	// Bit 7: Read only. Set to 1 when IRQ asserted. Clear when the
+	// control register associated with the channel that caused the
+	// interrut is read.
+	uint8_t m_m6844_interrupt;
+	uint8_t m_m6844_chain;
+	void m6844_update_interrupt();
+	void m6844_fdc_dma_transfer(uint8_t channel);
+	void m6844_hdc_dma_transfer(uint8_t channel);
+};
 
 // device type definition
 DECLARE_DEVICE_TYPE(MC6844, mc6844_device)
